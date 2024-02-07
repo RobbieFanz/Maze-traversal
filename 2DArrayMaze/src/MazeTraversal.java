f8da3ac4bdc4fca97f22c280d2f5cc97f48cb011
RobbieFanz <fanzuttir@gmail.com>
2024-02-07 16:08:54 -0500

    Initial commit

---
+++ 2DArrayMaze/src/MazeTraversal.java
@@ -0,0 +1,17 @@
+
+public class MazeTraversal {
+
+	public static void main(String[] args) {
+		
+		//two dimensional array with outer array as block and inner array as which directions are possible
+		final int[][] maze = {{1}, {1}, {1}, {1}, {1}};
+		
+		int start = 1;
+		int end = 16;
+		
+		System.out.println(maze[1][0]);
+		
+
+	}
+
+}
