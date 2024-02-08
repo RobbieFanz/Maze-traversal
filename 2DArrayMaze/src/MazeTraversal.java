7eb7424d488e3dd63d9ec882ddc133e6b5999bf4
RobbieFanz <fanzuttir@gmail.com>
2024-02-08 03:19:35 -0500

    created maze

--- 2DArrayMaze/src/MazeTraversal.java
+++ 2DArrayMaze/src/MazeTraversal.java
@@ -1,15 +1,23 @@
-
 public class MazeTraversal {
+	
+	
+	public static up(int location) {
+		
+	}
+	
 
 	public static void main(String[] args) {
 		
 		//two dimensional array with outer array as block and inner array as which directions are possible
-		final int[][] maze = {{1}, {1}, {1}, {1}, {1}};
+		final int[][] maze = {{0,1,0,1}, {1,1,0,0}, {1,1,0,0}, {1,0,0,1}, 
+				{0,1,1,0}, {1,1,0,0}, {1,0,0,1}, {0,0,1,1},
+				{0,1,0,0}, {1,1,0,1}, {1,1,1,0}, {1,0,1,0},
+				{0,1,0,0}, {1,1,1,0}, {1,1,0,0}, {1,0,0,0}};//Left, Right, Up, Down (1 is true 0 is false)
 		
-		int start = 1;
-		int end = 16;
+		int start = 0;
+		int end = 15;
 		
-		System.out.println(maze[1][0]);
+		
 		
 
 	}
