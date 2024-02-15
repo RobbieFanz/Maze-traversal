e71ff8fa4e63671f09b55e08d031f8fc4ab34bbe
RobbieFanz <fanzuttir@gmail.com>
2024-02-15 16:25:23 -0500

    created find method

--- 2DArrayMaze/src/MazeTraversal.java
+++ 2DArrayMaze/src/MazeTraversal.java
@@ -37,15 +37,34 @@ public class MazeTraversal {
 		}
 	}
 	
-	public static String find(int start, int end, int[] visited, int[][] maze) {
-		if (up(start)) {
-			
+	public static int[] find(int location, int end, int[] visited, int[][] maze) {
+		if (location == end) {
+			return visited;
 		}
+		
+		if (left(location) && visited[location-1]!=1) {
+			find(location-1); 
+		}
+		
+		if (right(location) && visited[location+1]!=1) {
+			find(location+1); 
+		}
+		
+		if (down(location) && visited[location+4]!=1) {
+			find(location+4); 
+		}
+		
+		if (up(location) && visited[location+4]!=1) {
+			find(location-4); 
+		}
+		
 	}
+	
+	
 	
 	public static void main(String[] args) {
 		
-		
+		int visited = new int[16];
 		
 		int start = 0;
 		int end = 15;
