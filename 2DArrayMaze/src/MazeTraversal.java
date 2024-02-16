f9f8b2d258db44f3e31b480932ad4c34c0e01872
RobbieFanz <fanzuttir@gmail.com>
2024-02-15 22:00:09 -0500

    got everything to work in find method

--- 2DArrayMaze/src/MazeTraversal.java
+++ 2DArrayMaze/src/MazeTraversal.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class MazeTraversal {
 	
 	
@@ -7,11 +9,11 @@ public class MazeTraversal {
 			{0,1,0,0}, {1,1,0,1}, {1,1,1,0}, {1,0,1,0},
 			{0,1,0,0}, {1,1,1,0}, {1,1,0,0}, {1,0,0,0}};//Left, Right, Up, Down (1 is true 0 is false)
 	
-	public static int left(int location) {
+	public static boolean left(int location) {
 		if (maze[location][0]==1) {
-			return (location-1);
+			return true;
 		}else {
-			return location;
+			return false;
 		}
 	}
 	public static boolean right(int location) {
@@ -37,37 +39,42 @@ public class MazeTraversal {
 		}
 	}
 	
-	public static int[] find(int location, int end, int[] visited, int[][] maze) {
+	public static void find(int location, int end, int[] visited) {
+		int visit[] = new int[21];
+		visit = visited.clone();
+		visit[location]=1; 
 		if (location == end) {
-			return visited;
+			System.out.println(Arrays.toString(visit));
 		}
 		
 		if (left(location) && visited[location-1]!=1) {
-			find(location-1); 
+			find(location-1,end,visit); 
 		}
 		
 		if (right(location) && visited[location+1]!=1) {
-			find(location+1); 
+			find(location+1,end,visit); 
 		}
 		
 		if (down(location) && visited[location+4]!=1) {
-			find(location+4); 
+			find(location+4,end,visit); 
 		}
 		
-		if (up(location) && visited[location+4]!=1) {
-			find(location-4); 
+		if (up(location) && visited[location-4]!=1) {
+			find(location-4,end,visit); 
 		}
 		
+		//System.out.println(Arrays.toString(visited) + "not possible");
 	}
 	
 	
 	
 	public static void main(String[] args) {
 		
-		int visited = new int[16];
-		
+		int visited[] = new int[16];
 		int start = 0;
 		int end = 15;
+		
+		find(start,end,visited);
 		
 		
 		
